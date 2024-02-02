0b2aeb9c969ca65267cd8f96b1720f3e3be9f01e
M-RB3 <rabea9220@gmail.com>
2024-02-02 18:24:40 +0400

    update explore

--- apps/multilist/widget/explore.jsx
+++ apps/multilist/widget/explore.jsx
@@ -1,19 +1,18 @@
 const accountId = props.accountId || context.accountId;
-const contract = "nft.yearofchef.near";
+const contract = "mint.yearofchef.near";
 const marketId = "simple.market.mintbase1.near";
 
 const AFFILIATE_ACCOUNT = props.affiliateAccount || "mintbase.near";
-const [data, setData] = useState([]);
+const [page, setPage] = useState(0);
 
-useEffect(() => {
-  asyncFetch("https://graph.mintbase.xyz", {
-    method: "POST",
-    headers: {
-      "mb-api-key": "anon",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: `query GetStoreNfts( 
+const data = fetch("https://graph.mintbase.xyz", {
+  method: "POST",
+  headers: {
+    "mb-api-key": "anon",
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    query: `query GetStoreNfts( 
       $offset: Int = 0 $condition: mb_views_nft_metadata_unburned_bool_exp ) 
       @cached 
       { mb_views_nft_metadata_unburned( where: $condition 
@@ -35,20 +34,16 @@ useEffect(() => {
        } 
      }
   `,
-      variables: {
-        condition: {
-          nft_contract_id: {
-            _in: contract,
-          },
+    variables: {
+      condition: {
+        nft_contract_id: {
+          _in: contract,
         },
       },
-    }),
-  }).then((data) => {
-    const nfts = data.body.data.mb_views_nft_metadata_unburned;
-    console.log(data);
-    setData(nfts);
-  });
-}, []);
+    },
+  }),
+});
+const nfts = data?.body?.data?.mb_views_nft_metadata_unburned;
 
 const YoctoToNear = (amountYocto) => {
   return new Big(amountYocto || 0).div(new Big(10).pow(24)).toString();
@@ -76,71 +71,232 @@ if (!data.ok) {
 }
 
 const size = "20em";
-console.log(data);
-return (
-  // data !== null ? (
+const NFTcard = styled.a`
+  display: flex;
+  width: 250px;
+  flex-direction: column;
+  border: 2px solid #ec2109;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px #ec20096b;
+  transition: all 300ms ease-in-out;
+  cursor: pointer;
+  overflow: hidden;
+  text-decoration: none;
+  :hover {
+    box-shadow: 0px 0px 20px #ec20096b;
+    text-decoration: none;
+  }
+  img {
+    height: 250px;
+    object-fit: cover;
+    position: relative;
+    :before {
+      content: " ";
+      display: block;
+      position: absolute;
+      height: 250px;
+      width: 250px;
+      background-image: url("https://image-cache-service-z3w7d7dnea-ew.a.run.app/small?url=https://ipfs.io/ipfs/QmYF3z6rGwRVRGHfWzRpd9QQQKTcA3zfNkBkkgw3kwmCbW");
+    }
+  }
+  .desc {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 1rem;
+  }
+  .tile {
+    color: black;
+  }
+  .listed {
+    font-size: 10px;
+    color: #6c757d;
+    margin-left: auto;
+  }
+`;
+
+const Pagination = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+  div {
+    border: 1px solid transparent;
+    background: #ec2109;
+    border-radius: 2px;
+    padding: 10px;
+    font-size: 12px;
+    color: white;
+    cursor: pointer;
+    :hover {
+      background: #ec200975;
+    }
+    &.active {
+      background: white;
+      color: #ec2109;
+      border-color: #ec2109;
+    }
+  }
+`;
+const perPage = 50;
+const paginations = [...Array(parseInt(nfts?.length / perPage) + 1).keys()];
+
+let lastElement = paginations[paginations.length - 1];
+const handlePainate = (to) => {
+  if (to !== "...") {
+    setPage(parseInt(to));
+  }
+};
+const Page = ({ children }) => {
+  console.log("page", page);
+  console.log("children", children);
+
+  return (
+    <div
+      onClick={() => handlePainate(children[0])}
+      className={`${children[0] + "" == page + "" ? "active" : ""}`}
+    >
+      {children[0]}
+    </div>
+  );
+};
+const Number = () => {
+  // return (
+  //   <Pagination>
+  //     {page === 0 && <Page>0</Page>}
+  //     {page > 1 && <Page>{page - 1}</Page>}
+  //     {page > 0 && lastElement > page && <Page>{page}</Page>}
+  //     {lastElement > page && <Page>{page + 1}</Page>}
+  //     {lastElement > page + 3 && <Page>...</Page>}
+  //     {lastElement !== page && <Page>{lastElement}</Page>}
+  //   </Pagination>
+  // );
+  console.log(page);
+  console.log("lastElement", lastElement);
+  if (page === 0) {
+    return (
+      <Pagination>
+        <Page>0</Page>
+        <Page>{page + 1}</Page>
+        <Page>...</Page>
+        <Page>{lastElement}</Page>
+      </Pagination>
+    );
+  } else if (page === lastElement) {
+    return (
+      <Pagination>
+        <Page>0</Page>
+        <Page>...</Page>
+        <Page>{page - 1}</Page>
+        <Page>{page}</Page>
+      </Pagination>
+    );
+  } else if (page + 1 === lastElement) {
+    return (
+      <Pagination>
+        <Page>0</Page>
+        <Page>...</Page>
+        <Page>{page - 1}</Page>
+        <Page>{page}</Page>
+        <Page>{lastElement}</Page>
+      </Pagination>
+    );
+  } else if (page + 1 < lastElement && page > 3) {
+    return (
+      <Pagination>
+        <Page>0</Page>
+        <Page>...</Page>
+        <Page>{page - 1}</Page>
+        <Page>{page}</Page>
+        <Page>{page + 1}</Page>
+        <Page>...</Page>
+        <Page>{lastElement}</Page>
+      </Pagination>
+    );
+  } else if (page < lastElement) {
+    return (
+      <Pagination>
+        <Page>0</Page>
+        <Page>{page}</Page>
+        <Page>{page + 1}</Page>
+        <Page>...</Page>
+        <Page>{lastElement}</Page>
+      </Pagination>
+    );
+  }
+};
+
+return nfts.length > 0 ? (
   <>
     <div className="d-flex gap-4 flex-wrap">
-      {data?.slice(0, 4).map((listing, i) => {
-        const priceYocto = listing.price
-          .toLocaleString()
-          .replace(/,/g, "")
-          .replace(/\s/g, "");
-        const priceNear = YoctoToNear(priceYocto);
-
+      {nfts.slice(page * perPage, (page + 1) * perPage).map((nft) => {
+        // let priceYocto = 0;
+        // if (nft.listed) {
+        //   priceYocto = nft.listed
+        //     .toLocaleString()
+        //     .replace(/,/g, "")
+        //     .replace(/\s/g, "");
+        // }
+        // const priceNear = YoctoToNear(priceYocto);
         return (
-          <div className="d-flex flex-column gap-1 w-15 p-3">
-            <a
-              href={`https://mintbase.xyz/meta/${listing.metadata_id}/`}
+          <div className="d-flex flex-column wrap gap-1 w-15 p-3">
+            <NFTcard
+              href={`https://mintbase.xyz/meta/${nft.metadataId.replace(
+                ":",
+                "%3A"
+              )}`}
               target="_blank"
             >
-              <Widget
-                src="mob.near/widget/NftImage"
-                props={{
-                  nft: {
-                    tokenId: listing.token_id,
-                    contractId: listing.nft_contract_id,
-                  },
-                  style: {
-                    width: size,
-                    height: size,
-                    objectFit: "cover",
-                    minWidth: size,
-                    minHeight: size,
-                    maxWidth: size,
-                    maxHeight: size,
-                    overflowWrap: "break-word",
-                  },
-                  className: "",
-                  fallbackUrl:
-                    "https://ipfs.near.social/ipfs/bafkreihdiy3ec4epkkx7wc4wevssruen6b7f3oep5ylicnpnyyqzayvcry",
-                }}
+              <img
+                src={
+                  "https://image-cache-service-z3w7d7dnea-ew.a.run.app/media?url=" +
+                  nft.media
+                }
+                alt={nft.title}
               />
-            </a>
-            <button
-              disabled={!accountId}
-              onClick={() => {
-                if (!accountId) return;
-                buy(priceYocto, listing.token_id, listing.nft_contract_id);
-              }}
-              className="btn-main"
-              style={{
-                width: "100%",
-                textAlign: "cetner",
-              }}
-            >
-              Buy {priceNear} N
-            </button>
+              <div className="desc">
+                <div className="tile">{nft.title}</div>
+                {!nft.listed && <div className="listed">not listed</div>}
+              </div>
+            </NFTcard>
+            {/* {!nft.listed && (
+              <button
+                disabled={!accountId}
+                onClick={() => {
+                  if (!accountId) return;
+                  buy(priceYocto, listing.token_id, listing.nft_contract_id);
+                }}
+                className="btn-main"
+                style={{
+                  width: "100%",
+                  textAlign: "cetner",
+                }}
+              >
+                Buy {priceNear} N
+              </button>
+            )} */}
           </div>
         );
       })}
     </div>
-    {JSON.stringify(data)}
-    <div class="py-4">
-      <Widget src="mintbase.near/widget/BuiltWithMintbase" />
-    </div>
+    {
+      <Number />
+      // <Pagination>
+      //   {/* <Page>0</Page>
+      //   <Page>1</Page>
+      //   <Page>2</Page>
+      //   <Page>...</Page>
+      //   <Page>{lastElement - 1}</Page>
+      //   <Page>{lastElement}</Page> */}
+      //   {page === 0 ? <Page>0</Page> : ""}
+      //   {/* {page > 1 && <Page>{page - 1}</Page>} */}
+      //   {/* {page > 0 && <Page>{page}</Page>} */}
+      //   {/* {lastElement > page && <Page>{page + 1}</Page>} */}
+      //   {/* {lastElement > page + 3 && <Page>...</Page>} */}
+      //   {lastElement !== page && <Page>{lastElement}</Page>}
+      // </Pagination>
+    }
+    {/* <Number /> */}
   </>
+) : (
+  <p>loading...</p>
 );
-// ) : (
-//   <p>loading...</p>
-// );
